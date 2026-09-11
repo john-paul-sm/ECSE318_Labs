f758499716dbda33215125ad4f329369178f073a
john-paul-sm <johnp.magbitang@gmail.com>
2026-09-11 03:31:19 -0400

    created the structural full adder module

--- Lab1/Problem1/P1_Structural.v
+++ Lab1/Problem1/P1_Structural.v
@@ -1,18 +1,18 @@
 module full_adder (output sum, output cout, input a, input b, input cin);
 
-    wire p, q, r, s;
+    wire s1, c1, c2, c3;
 
     // sum = a ⊕ b ⊕ cin
 
-    xor g_x0(p, a, b);
-    xor g_x1(sum, cin, p);
+    xor g_x0(s1, a, b);
+    xor g_x1(sum, cin, s1);
     
-    // cout = a * b || a & cin || b & cin
+    // cout = a * b | a & cin | b & cin
 
-    and g_a0(q, a, b);
-    and g_a1(r, a, cin);
-    and g_a2(s, b, cin);
+    and g_a0(c1, a, b);
+    and g_a1(c2, a, cin);
+    and g_a2(c3, b, cin);
 
-    or (cout, q, r, s);
+    or g_o0(cout, c1, c2, c3);
 
 endmodule
