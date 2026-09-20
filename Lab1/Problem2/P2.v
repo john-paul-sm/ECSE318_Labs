eb2d658863a78ee57c99e9fe833d1231254591a1
Alexander Enriquez <axe317@case.edu>
2026-09-19 20:49:25 -0400

    update P2.v comments

--- Lab1/Problem2/P2.v
+++ Lab1/Problem2/P2.v
@@ -8,7 +8,7 @@ input [3:0] A, B;
 wire [3:0] P, G;
 wire C1, C2, C3, C4;
 
-// PG Generator (Each gate done individually)
+// PG Generator (Each gate done individually for "progagate" and "generate" signals)
 xor #10 p3(P[3], A[3], B[3]);
 xor #10 p2(P[2], A[2], B[2]);
 xor #10 p1(P[1], A[1], B[1]);
@@ -19,7 +19,7 @@ and #10 g2(G[2], A[2], B[2]);
 and #10 g1(G[1], A[1], B[1]);
 and #10 g0(G[0], A[0], B[0]);
 
-// Carry Generate Block (Each "signal" done individually)
+// Carry Generate Block (Each "carry" signal done individually)
 // CI
 wire temp1;
 and #10 c1a1(temp1, P[0], c0);
@@ -48,7 +48,7 @@ or #10 c4s(C4, G[3], temp7, temp8, temp9, temp10);
 
 assign cout = C4;
 
-// Sum Generator (Each gate done individually)
+// Sum Generator (Each "sum" signal done individually)
 xor #10 s0(sum[0], P[0], c0);
 xor #10 s1(sum[1], P[1], C1);
 xor #10 s2(sum[2], P[2], C2);
